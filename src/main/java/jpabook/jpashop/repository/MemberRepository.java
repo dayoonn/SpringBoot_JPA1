ee8cf51270be8230d99de2f59d28dfb6dcf83855
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-06-03 22:09:02 +0900

    생성자 주입 변경

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,15 +3,16 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em; //생성자 주입 (롬복)
 
     public void save(Member member){
         em.persist(member);
