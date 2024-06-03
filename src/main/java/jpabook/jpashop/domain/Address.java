c35b3cd77ee04b9e317a68732ce8804e1667d605
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-06-03 18:13:24 +0900

    엔티티 생성

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable //임베디드 내장 타입 포함
-@Getter @Setter
+@Getter
 public class Address {
-
     private String city;
     private String street;
     private String zipcode;
+    protected Address() {
+    }
+    public Address(String city, String street, String zipcode) { //생성자에서 값을 모두 초기화
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
