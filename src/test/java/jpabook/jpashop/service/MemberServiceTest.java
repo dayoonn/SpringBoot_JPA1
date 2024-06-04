5b4d2f899f59ab2d59ace6200b9315ae959c4c99
dayoon <102327475+dayoonn@users.noreply.github.com>
2024-06-04 21:51:32 +0900

    test : 회원 서비스 테스트 코드 생성
    - 회원가입 테스트
    - 중복 회원 예외 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) //JUnit에게 스프링과 관련된 걸로 테스트 한다는 것을 알림
+@SpringBootTest  //스프링부트로 테스트 돌림
+@Transactional //테스트 후 디비 롤백
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+
+        assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //예외 발생 !
+
+
+        //then
+        fail("예외가 발생해야 한다."); //Assert 메서드
+
+    }
+
+}
